6352a2aea24d4e3b5878f45a8063cbe1c42fdd1b
ert-developer <ert.developer@gmail.com>
2025-05-27 13:34:08 +0530

    chore: Update Vite configuration and Firebase hosting workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
